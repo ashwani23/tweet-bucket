144f110169d04c0dfd0ae4bc2c730fdc673ed79a
ashwani23 <ashwanigoyal@outlook.in>
2017-04-02 15:31:59 +0530

    Final changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,38 @@
 jQuery(document).ready(function(){
-    jQuery.ajax({
-        method : 'GET',
-        url : 'api/tweets.php',
-        dataType : "json",
-        data : {'auth_token' : '82a27432347dc1ba414bf7219d729ea1'},
-        success : function(response){
-            var htmlString = '';
-            if(typeof response != 'undefined'){
-                jQuery.each(response.data, function(tweetIndex, tweet){
-                    htmlString += '<div class="col-md-6 tweet"><div class="testimonials"> <div class="active item"><blockquote><p><span class="badge">'+tweet.tweetCount+'</span>&nbsp;'+tweet.tweetText+'</p><p><span class="label label-success">Retweet(s): '+tweet.retweetCount+'</span><span class="label label-warning">'+tweet.createdAtNiceTimeStamp+'</span></p></blockquote><div class="carousel-info"><img alt="" src="'+tweet.profileImageUrl+'" class="pull-left"><div class="pull-left"><span class="testimonials-name">'+tweet.userName+'</span><span class="label label-primary"># '+tweet.userScreenName+'</span></div></div></div></div></div></div>';
-                });
-            } else {
-                htmlString = '<div class="alert alert-danger">No matching tweet was found</div>';
+
+    /**
+     * To call API endpoint and fetch tweets.
+     * Then create HTML content
+     */
+    function callForTweets(){
+        jQuery.ajax({
+            method : 'GET',
+            url : 'api/tweets.php',
+            dataType : "json",
+            data : {'auth_token' : '82a27432347dc1ba414bf7219d729ea1'},
+            success : function(response){
+                var htmlString = '';
+
+                /**
+                 * Check if response is observed
+                 */
+                if(typeof response != 'undefined'){
+                    jQuery.each(response.data, function(tweetIndex, tweet){
+                        htmlString += '<div class="col-md-6 tweet"><div class="testimonials"> <div class="active item"><blockquote><p><span class="badge">'+tweet.tweetCount+'</span>&nbsp;'+tweet.tweetText+'</p><p><span class="label label-success">Retweet(s): '+tweet.retweetCount+'</span><span class="label label-warning">'+tweet.createdAtNiceTimeStamp+'</span></p></blockquote><div class="carousel-info"><img alt="" src="'+tweet.profileImageUrl+'" class="pull-left"><div class="pull-left"><span class="testimonials-name">'+tweet.userName+'</span><span class="label label-primary"># '+tweet.userScreenName+'</span></div></div></div></div></div></div>';
+                    });
+                } else {
+                    htmlString = '<div class="alert alert-danger">No matching tweet was found</div>';
+                }
+                jQuery('#tweets').html(htmlString);
             }
-            jQuery('#tweets').html(htmlString);
-        }
+        });
+    }
+
+    /* Load Tweets on page load */
+    callForTweets();
+
+    jQuery('#refreshTweets').click(function(){
+        callForTweets();
     });
+
 });
