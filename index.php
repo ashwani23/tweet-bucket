4ac8c222967aac2a4e29324ad8110ef31579af36
ashwani23 <ashwanigoyal@outlook.in>
2017-04-02 10:38:56 +0530

    Installing Bootstrap via NPM

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+  * index.php
+  *
+  * @author     Ashwani Goyal <ashwanigoyal@outlook.in>
+  */
+
+ include_once 'vendor/autoload.php';
+
+/*
+Loading ENV Parameters
+ */
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
